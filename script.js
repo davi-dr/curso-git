b019131642c76325d2de749ad0c5e410632074fe
DAVI DOUGLAS PEREIRA RODRIGUES <davi.dprodrigues001@gmail.com>
2024-05-30 16:05:57 -0300

    Inserido mais textos na lista e adicionado comentários no script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Seja Bem Vindo !");
+// alert("Seja Bem Vindo !");
+
+/* COMANDOS GIT :
+git init : Inicia um novo repositório vazio no GIT local na pasta atual.
+
+git status : Verifica o status atual do repositório, incluindo arquivos modificados e arquivos na (Staging Area).
+
+git commit -m : Grava as alterações feitas na área de stage como uma nova versão do código.
+
+OBS: Mensagem de Commit: É comum usar -m "Mensagem do commit" junto com git commit -a para adicionar uma mensagem de commit, como em git commit -a -m "Atualizações de arquivos rastreados".
+
+git add + NomeArquivo : Adiciona arquivos selecionados para a área de stage (pronto para ser commitado).
+
+git add . : Adicionar todos os arquivos.
+
+git log : Ver o histórico de commits.
+
+git checkout: Usado para alternar entre BRANCHS, cria novos branches, ou mudar para uma versão anterior do codigo em um branch existente.
+
+git branch: Lista as branches do projeto.
+
+git merge: */
